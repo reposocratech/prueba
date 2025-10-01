5db137d10fece03f40e07dfbe7806faf963071f6
ciprian <pascubarbciprian@gmail.com>
2025-10-01 09:35:38 +0200

    Bienvenido

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("hola");
 console.log("Adios");
 
+
+import React from 'react'
+<div>
+<h1>Bienvenid@s a mi app</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut sunt tempora itaque ab id odit, dolore mollitia provident in nostrum esse voluptate perferendis non necessitatibus, eligendi nesciunt laboriosam. Ab, nihil.</p>
+</div>
