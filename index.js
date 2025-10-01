d77e5ab1000408e6b253013ee8ac85ed26bdfc18
Amanda Gómez Quero <amanda1758@gmail.com>
2025-10-01 09:35:21 +0200

    'primeraRama'

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("hola");
 console.log("Adios");
 
+console.log('holiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+console.log('soy Amanda')
+/* estamos dando una clase con Santi */
+console.log('vamos a subir una rama')
