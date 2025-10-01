c16768b79ec6ad858d73a07d265d9dc1d5365f11
Soufian Younoussi Azibi <soufian.younoussi@gmail.com>
2025-10-01 09:34:48 +0200

    poca cosa

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 console.log("hola");
 console.log("Adios");
 
+
+const sumar = (a,b) => {
+    return a + b
+}
+console.log(sumar(10, 15))
+
+let cosas = ["dibujo", "código", "crossfit"]
+
+let verCosas = cosas.map(e => console.log(e))
